a496a7acabe75f5c412af2bf86aa9d467c17c18f
Daniel <d.andres.bacho@gmail.com>
2024-03-25 16:26:05 +0100

    Initial commit

---
+++ src/main/java/org/example/pocspringsecurityregisteruser/PocSpringSecurityRegisterUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.pocspringsecurityregisteruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringSecurityRegisterUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocSpringSecurityRegisterUserApplication.class, args);
+    }
+
+}
