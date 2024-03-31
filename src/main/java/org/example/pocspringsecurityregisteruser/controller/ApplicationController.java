ed22c82dd367b481a51498ab704bb15a5d62f9c5
Daniel <d.andres.bacho@gmail.com>
2024-03-31 14:51:28 +0200

    Implementación de las pantallas de Login, Home y Register

---
+++ src/main/java/org/example/pocspringsecurityregisteruser/controller/ApplicationController.java
@@ -0,0 +1,19 @@
+package org.example.pocspringsecurityregisteruser.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ApplicationController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegisterPage(){
+        return "register";
+    }
+}
