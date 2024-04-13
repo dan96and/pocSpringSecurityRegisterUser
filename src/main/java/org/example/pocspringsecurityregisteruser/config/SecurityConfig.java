4a126d5c6fbb8b3f604acfbd9c3bdd5fe88bb78b
Daniel <d.andres.bacho@gmail.com>
2024-04-13 18:11:12 +0200

    Eliminación de código Javascript

--- src/main/java/org/example/pocspringsecurityregisteruser/config/SecurityConfig.java
+++ src/main/java/org/example/pocspringsecurityregisteruser/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/register", "/create-user").permitAll()
+                .requestMatchers("/login", "/register", "/create-user", "/home").permitAll()
                 .anyRequest().authenticated());
 
         http.formLogin(login -> login
