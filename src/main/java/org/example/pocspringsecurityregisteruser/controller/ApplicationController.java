054e4f6d43e9ebee964bbf04b7b3cf8ddd7468d2
Daniel <d.andres.bacho@gmail.com>
2024-05-29 15:30:36 +0200

    update

--- src/main/java/org/example/pocspringsecurityregisteruser/controller/ApplicationController.java
+++ src/main/java/org/example/pocspringsecurityregisteruser/controller/ApplicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-
+//update
 public class ApplicationController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String showLoginPage() {
